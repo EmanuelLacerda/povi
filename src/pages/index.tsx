12e119bc344d83fc095cad063de3017d25f594c7
E3S23L <emanuelifpb@gmail.com>
2021-02-26 13:28:40 -0300

    style: Moves the styles of the Home from global.css to a CSS Module.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import styles from '../styles/pages/Home.module.css'
 import { ExperienceBar } from '../components/ExperienceBar'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ExperienceBar />
+
+      <section>
+      	<div>
+      		
+      	</div>
+      	<div>
+
+      	</div>
+      </section>
     </div>
   )
 }
