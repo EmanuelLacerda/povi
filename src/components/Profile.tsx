7419db428d888800e4ac824bebf14453de8a4de2
E3S23L <emanuelifpb@gmail.com>
2021-02-26 14:01:01 -0300

    chore: Adds level icon.

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,10 @@ export function Profile(){
 			<img src="https://github.com/EmanuelLacerda.png" alt="Emanuel Lacerda" />
 			<div>
 				<strong>Emanuel Lacerda</strong>
-				<p>Level 1</p>
+				<p>
+					<img src="icons/level.svg" alt="Level" />
+					Level 1
+				</p>
 			</div>
 		</div>
 	);
