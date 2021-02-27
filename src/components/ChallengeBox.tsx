19316a185c59cbf60e272a0f58485d7c05bb318b
E3S23L <emanuelifpb@gmail.com>
2021-02-27 17:13:26 -0300

    style: Stylizes challenge box not active.

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,7 +1,9 @@
+import styles from '../styles/components/ChallengeBox.module.css'
+
 export function ChallengeBox(){
 	return (
-		<div>
-			<div>
+		<div className={styles.challengeBoxContainer}>
+			<div className={styles.challengeNotActive}>
 				<strong>Finalize um ciclo para receber um desafio</strong>
 				<p>
 					<img src="icons/level-up.svg" alt="Level Up" />
