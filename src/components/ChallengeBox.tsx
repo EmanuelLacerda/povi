854461e509380b271abd37711d5077093996adb5
E3S23L <emanuelifpb@gmail.com>
2021-02-27 16:30:16 -0300

    feat: Adds challenge box not active.

---
+++ src/components/ChallengeBox.tsx
@@ -0,0 +1,13 @@
+export function ChallengeBox(){
+	return (
+		<div>
+			<div>
+				<strong>Finalize um ciclo para receber um desafio</strong>
+				<p>
+					<img src="icons/level-up.svg" alt="Level Up" />
+					Avance de level completando desafios.
+				</p>
+			</div>
+		</div>
+	)
+}
