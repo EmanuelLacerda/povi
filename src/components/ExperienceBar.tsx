8e396664cb516231c658f0850e40c7b6eb6d9419
E3S23L <emanuelifpb@gmail.com>
2021-03-02 14:07:15 -0300

    feat(experiencebar): adds context of the challenges

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,17 +1,25 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+
 import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar(){
+	const { experienceToNextLevel, currentExperience } = useContext(ChallengesContext);
+
+	let percentualToNextLevel = (currentExperience/experienceToNextLevel)*100;
+	console.log(`{percentualToNextLevel}%`);
+
 	return (
 		<header className={styles.experienceBar}>
 			<span>0 xp</span>
 			<div>
-				<div style={{width: '50%'}}> </div>
+				<div style={{width: `${percentualToNextLevel}%`}}> </div>
 
-				<span className={styles.currentExperience} style={{left: '50%'}}>
-					300 xp
+				<span className={styles.currentExperience} style={{left: `${percentualToNextLevel}%`}}>
+					{currentExperience} xp
 				</span>
 			</div>
-			<span>600 xp</span>
+			<span>{experienceToNextLevel} xp</span>
 		</header>
 	)
 }
