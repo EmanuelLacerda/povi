1ff5ea079df5414273aba511e0001202b2f4d612
E3S23L <emanuelifpb@gmail.com>
2021-03-03 11:29:51 -0300

    feat(Countdown): Re-adds contexts.
    In previous commits Countdown contexts were added and now it was being pointed out as modified. This commit will then add the contexts to Countdown.

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,38 +1,23 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
+import { CountdownContext } from '../contexts/CountdownContext'
+
 import styles from '../styles/components/Countdown.module.css'
 import { useState, useEffect } from 'react';
 
-let countdownTimeout: NodeJS.Timeout;
-
 export function Countdown(){
-	const [ time, setTime ] = useState(0.1*60);
-	const [isActive, setIsActive] = useState(false);
-	const [hasFinished, setHasFinished] = useState(false)
-
-
-	const minutes = Math.floor(time / 60);
-	const seconds = time % 60;
+	const { time, minutes,seconds, isActive, hasFinished, decreaseTime, endCycle, startCountdown, resetCountdown } = useContext(CountdownContext);
+	const { startNewChallenge } = useContext(ChallengesContext);
 
 	const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('');
 	const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
-	function startCountdown(){
-		setIsActive(true);
-	}
-
-	function resetCountdown(){
-		clearTimeout(countdownTimeout);
-		setIsActive(false);
-		setTime(0.1*60);
-	}
-
 	useEffect(() => {
 		if (isActive && time > 0){
-			countdownTimeout = setTimeout(() => {
-				setTime(time-1);
-			}, 1000) 
+			decreaseTime();
 		}  else if (time == 0){
-			setHasFinished(true);
-			setIsActive(false);
+			endCycle();
+			startNewChallenge();
 		}
 	}, [isActive, time])
 	
