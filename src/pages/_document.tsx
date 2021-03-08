4fdad780c45a7e0abe95385a8a48585d96147f4d
E3S23L <emanuelifpb@gmail.com>
2021-03-08 15:01:27 -0300

    chore: Adds externas fonts.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,6 +9,9 @@ export default class MyDocument extends Document{
 
 				    <link rel="preconnect" href="https://fonts.gstatic.com" />
 				    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+				
+				    <link rel="preconnect" href="https://fonts.gstatic.com" />
+					<link href="https://fonts.googleapis.com/css2?family=Baloo+2:wght@600&family=Hind+Siliguri:wght@400;500;600&display=swap" rel="stylesheet" />
 				</Head>
 				<body>
 					<Main />
